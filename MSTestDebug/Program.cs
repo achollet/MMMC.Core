67f396c2cc684a0952514785268aa4e8b0307e16
achollet <Alexis@mbp-de-alexis.home>
2017-11-05 18:50:54 +0100

    Adding console project for debugging Test

---
+++ MSTestDebug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSTestDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
